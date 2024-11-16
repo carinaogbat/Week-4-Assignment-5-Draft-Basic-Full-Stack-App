3e4254177805f04f833d81c46644b2fd781e3610
Carina Ogbat <carinawrightogbat@gmail.com>
2024-11-16 11:34:26 -0800

    updated functions

--- my-app/lib/data.js
+++ my-app/lib/data.js
@@ -1,86 +1,117 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 // data.js is utility functions to read data
-
+import got from 'got';
 //get filepath to data directory. cwd = current working directory
-const dataDir = path.join( process.cwd(), 'data' );
+// const dataDir = path.join( process.cwd(), 'data' );
 
-export function getSortedData() {
-    // get filepath to json file
-    const filePath = path.join(dataDir, 'cats.json');
+// define url from rest endpoint
+const dataURL = "https://dev-cs5033-carinaogbat.pantheonsite.io/wp-json/twentytwentyone-child/v1/latest-posts/1"
 
-    // load json file contents
-    const jsonString = fs.readFileSync( filePath, 'utf8');
+export async function getSortedData() {
+  // get filepath to json file
+  // const filePath = path.join(dataDir, 'persons.json');
 
-    // convert string from file into json array object
-    const jsonObj = JSON.parse(jsonString);
+  // load json file contents
+  // const jsonString = fs.readFileSync(filePath, 'utf8');
+  let jsonString;
+  try {
+    // next line uses got synchronously to retrive via https our json data from wp site
+    jsonString = await got(dataURL);
+    console.log(jsonString.body);
+  } catch(error) {
+    jsonString.body = [];
+    console.log(error);
+  }
+  
+  // convert string from file into json array object
+  // const jsonObj = JSON.parse(jsonString);
+  const jsonObj = JSON.parse(jsonString.body);
 
-    // sort json array by name property
-    jsonObj.sort(
-        function (a, b) {
-            return a.name.localeCompare(b.name);
-        }
-    );
+  // sort json array by name property
+  jsonObj.sort(function (a, b) {
+      return a.post_title.localeCompare(b.post_title);
+  });
 
-    // use map() on array to exract the id and name properties into a new array of object values
-    return jsonObj.map(
-        function(item) {
-            return {
-                id: item.id.toString(),
-                name: item.name,
-            };
-        }
-    );
+  // use map() on array to extract just id + name properties into new array of obj values
+  return jsonObj.map(item => {
+    return {
+      id: item.ID.toString(),
+      name: item.post_title
+    }
+  });
 }
 
+
+
+
 // function to return all cat ids
-export function getCatIds() {
-        // get filepath to json file
-        const filePath = path.join(dataDir, 'cats.json');
+export async function getCatIds() {
 
-        // load json file contents
-        const jsonString = fs.readFileSync( filePath, 'utf8');
-    
-        // convert string from file into json array object
-        const jsonObj = JSON.parse(jsonString);
+  let jsonString;
+  try {
+    // next line uses got synchronously to retrive via https our json data from wp site
+    jsonString = await got(dataURL);
+    console.log(jsonString.body);
+  } catch(error) {
+    jsonString.body = [];
+    console.log(error);
+  }
 
-        return jsonObj.map(
-            function(item) {
-                return {
-                    params: {
-                    id: item.id.toString()
-                    }
-                };
-            }
-        );
+
+  const jsonObj = JSON.parse(jsonString.body);
+
+  // use map() on array to extract just id properties into new array of obj values
+  return jsonObj.map(item => {
+    return {
+      params: {
+        id: item.ID.toString()
+      }
+    }
+  });
+
 }
 
 // return all of the properties for the object with a matching id value
 export async function getData( requestedID ) {
-        // get filepath to json file
-        const filePath = path.join(dataDir, 'cats.json');
+ // get filepath to json file
+  // const filePath = path.join(dataDir, 'persons.json');
 
-        // load json file contents
-        const jsonString = fs.readFileSync( filePath, 'utf8');
-    
-        // convert string from file into json array object
-        const jsonObj = JSON.parse(jsonString);
+  // load json file contents
+  // const jsonString = fs.readFileSync(filePath, 'utf8');
+  let jsonString;
+  try {
+    // next line uses got synchronously to retrive via https our json data from wp site
+    jsonString = await got(dataURL);
+    console.log(jsonString.body);
+  } catch(error) {
+    jsonString.body = [];
+    console.log(error);
+  }
 
-        const matchingObj = jsonObj.filter(
-            function(obj) {
-                return obj.id.toString() === requestedID;
-            }
-        );
+  // convert string from file into json array object
+  // const jsonObj = JSON.parse(jsonString);
+  const jsonObj = JSON.parse(jsonString.body);
 
-        let returnedObj;
-        if (matchingObj.length > 0) {
-            returnedObj = matchingObj[0] 
-        } else {
-            returnedObj = {};
-        }
+  // find object value in array that has matching id
+  const objMatch = jsonObj.filter(obj => {
+    return obj.ID.toString() === idRequested;
+  });
 
-        return returnedObj;
+  // extract object value in filtered array if any
+  let objReturned;
+  if (objMatch.length > 0) {
+    objReturned = objMatch[0];
+  } else {
+    objReturned = {};
+  }
+  // console.log(objReturned);
+
+  // return object value found
+  return objReturned;
 }
+
+   
 
 // return all of the properties for the object with a matching id value
 export async function getFilePath( requestedID ) {
@@ -95,7 +126,7 @@ export async function getFilePath( requestedID ) {
 
     const matchingObj = jsonObj.filter(
         function(obj) {
-            return obj.id.toString() === requestedID;
+            return obj.ID.toString() === requestedID;
         }
     );
 
